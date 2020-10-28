1fcbfdf996a56ca77aafafdd26184c152ea13359
EltonChou <ie9181124@gmail.com>
2020-10-28 20:49:14 +0800

    [FIX] it might be better after use user-agent

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -38,7 +38,9 @@ def test_include_fld():
     }
 
     nmq = NoMoreQS(include_flds=('youtube.com'))
-    assert nmq.clean(url["input"]) == url["output"]
+    assert nmq.clean(url["input"], headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+    }) == url["output"]
 
 
 def test_remove_fbclid():
